8429f7ff5765aa79c04c4c5ed09b0a101e1fddc7
Erik Costa <cerik1632@gmail.com>
2026-08-31 23:28:49 -0300

    Delete dados_usuario.py

--- dados_usuario.py
+++
@@ -1,5 +0,0 @@
-nome = input("Digite seu nome: ")
-curso = input("Digite seu curso: ")
-periodo = input("Digite seu período atual: ")
-
-print("O(A) estudante ", nome, " está matriculado(a) no curso de ", curso, ", cursando o ", periodo, " período.")
