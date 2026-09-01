543ee54b6a2650cb32c21e70bebdc7835ebf4205
Erik Costa <cerik1632@gmail.com>
2026-08-31 23:29:14 -0300

    Delete media_aluno.py

--- media_aluno.py
+++
@@ -1,7 +0,0 @@
-nome = input("Digite o nome do aluno: ")
-nota1 = float(input("Digite a primeira nota: "))
-nota2 = float(input("Digite a segunda nota: "))
-nota3 = float(input("Digite a terceira nota: "))
-
-media = (nota1 + nota2 + nota3) / 3
-print("O aluno", nome, "obteve média final:", media)
