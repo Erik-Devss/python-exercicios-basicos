082e70b782d3a7f2bd21018cba1ffc7c25ba0ba1
Erik Costa <cerik1632@gmail.com>
2026-08-31 23:26:42 -0300

    Delete exercicios directory

--- exercicios/limite_faltas.py
+++
@@ -1,8 +0,0 @@
-aluno = input("Digite o nome do aluno: ")
-faltas = int(input("Digite o número de faltas: "))
-if faltas > 10:
-    print(f"O aluno {aluno} está acima do limite de faltas. Número de faltas: {faltas}.")
-elif faltas == 10:
-    print(f"O aluno {aluno} está no limite de faltas.")
-else:
-    print(f"O aluno {aluno} está abaixo do limite de faltas. Número de faltas: {faltas}.")
--- exercicios/senha.py
+++
@@ -1,6 +0,0 @@
-senha = input("Digite a senha: ")
-senha_correta = "python123"
-if senha == senha_correta:
-    print("Acesso permitido.")
-else:
-    print("Acesso negado. Senha incorreta.")
